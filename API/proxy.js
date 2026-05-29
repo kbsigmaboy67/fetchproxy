28fc49d25dc6b1c3729498e74a223da35eb3a503
kbsigmaboy67 <kbsigmaboy67@gmail.com>
2026-05-29 12:23:07 -0600

    Update proxy.js

--- API/proxy.js
+++ API/proxy.js
@@ -1,25 +1,20 @@
 export default async function handler(req, res) {
+  const path = req.query.path || "";
+
+  const target = `https://kbsigmaboy67.github.io/${path}`;
+
   try {
-    const path = req.query.path || "";
+    const response = await fetch(target);
 
-    const targetUrl = `https://kbsigmaboy67.github.io/${path}`;
-
-    const response = await fetch(targetUrl, {
-      method: req.method,
-      headers: {
-        "User-Agent": "Vercel-Proxy"
-      }
-    });
-
-    const contentType = response.headers.get("content-type") || "text/plain";
-    const data = await response.text();
+    const text = await response.text();
 
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Content-Type", contentType);
+    res.setHeader(
+      "Content-Type",
+      response.headers.get("content-type") || "text/plain"
+    );
 
-    res.status(response.status).send(data);
+    res.status(response.status).send(text);
   } catch (err) {
     res.status(500).json({
       error: err.message
